d768535e30c2d505c834f4f28610019f0c14c824
Mirelatsonova <37420597+Mirelatsonova@users.noreply.github.com>
2018-04-02 15:43:52 +0200

    Hausaufgabe

--- Problem_1.R
+++ Problem_1.R
@@ -1,1 +1,34 @@
+#Problem 1
+
+#a)
+x <- c (4,1,1,4)
+
+#b)
+y <- c(1,4)
+
+#c) Ot x se izvajda vseki element ot y spored reda na zapisvane na elementite.
+# Na poslednite dve pozitsii ot razlikata stoqt nyli, 
+# tui kato vektorut y e samo s dva elementa
+# i nqma kak da se izvurshi izvajdane s lipsvasht element.
+
+difference <- x - y
+
+#d) 
+s <- c (x,y)
+
+#e)
+rep (s,10)
+length (rep(s,10))
+
+#f)
+rep (s, each=3)
+
+#g)
+seq(7,21, by=1)
+7:21
+
+#h)
+
+length(seq(7,21, by=1))
+
 
--- Problem_2.R
+++ Problem_2.R
@@ -1,1 +1,51 @@
+#Problem 2
 
+#a)
+xmin <- c (23.0, 20.5, 28.2, 20.3, 22.4, 17.2, 18.2)
+xmax <- c (25.0, 22.8, 31.2, 27.3, 28.4, 20.2, 24.1)
+
+#b)
+TempDifference <- xmax - xmin
+
+#c) 
+avgMin <- mean (xmin)
+avgMax <- mean (xmax)
+
+#d)
+xmin [c(xmin < avgMin)]
+
+#e) 
+xmax [c(xmax > avgMax)]
+
+#f)
+date <- c('3Mon18', '4Tue18', '5Wed18', '6Thu18', '7Fri18', '8Sat18', '9Sun18')
+names(xmin) <- date
+names (xmax) <- date
+
+#g)
+temperatures <- data.frame(
+  xmin=xmin,
+  xmax=xmax
+)
+
+#h)
+temperatures <- within (temperatures,{
+  xminFahrenheit <- xmin*9/5 +32
+})
+
+#i)
+temperaturesFahrenheit <- data.frame(
+  day=date,
+  xminFahrenheit=temperatures$xminFahrenheit
+)
+
+#j)
+tempFahrenheitBeginning <- data.frame(
+ temperatures = temperaturesFahrenheit [c(1:5),]
+)
+
+#or
+
+tempFahrenheitBeginning1 <- data.frame(
+  temperatures = temperaturesFahrenheit [-c(6,7),]
+)
